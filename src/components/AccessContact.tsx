6d3aa012bfa9c7337574d7e22316c535bee7986f
HISASHIKUSANO <hisashi.kusano@example.com>
2025-06-14 18:09:04 +0900

    fix: correct Google Map embed

--- src/components/AccessContact.tsx
+++ src/components/AccessContact.tsx
@@ -22,7 +22,7 @@ const AccessContact: React.FC = () => (
       </div>
       <motion.iframe
         title="map"
-        src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d1677.9054180530132!2d133.083!3d34.404!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sja!2sjp!4v000000000"
+        src="https://maps.google.com/maps?q=%E5%B0%BE%E9%81%93%E5%B8%82%E5%9B%A0%E5%B3%B6%E5%9C%9F%E7%94%9F%E7%94%BA2205-1&hl=ja&z=17&output=embed"
         className="w-full h-64 md:h-full rounded-2xl shadow-md border-0"
         loading="lazy"
         allowFullScreen
